311a9102abb62afd83a8e85a03792f54950e9231
Anmol kumar <anmolkumar31884@gmail.com>
2024-09-10 16:10:30 +0530

    index file created

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
-const express=require('express')
-const routes = require('./src/routes/routes')
 require('dotenv').config()
+const express=require('express')
+const routes = require('./src/routes/Routes')
+const DbConnect=require('./src/config/DBConnect')
 
 const app=express()
 
 app.listen(process.env.PORT,()=>{
     console.log('app is running on port',process.env.PORT)
 })
+DbConnect()
 app.use('/api/v1',routes)
-// app.get('/',(req,res)=>{
-//     res.send('hello')
-// })
