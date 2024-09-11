d2ade4b26cedc7e0147b57f1df04df78cd112e1d
Anmol kumar <anmolkumar31884@gmail.com>
2024-09-11 22:49:06 +0530

    cloudinary image uploader added

---
+++ server/src/utils/Cloudinary.js
@@ -0,0 +1,20 @@
+const cloudinary = require('cloudinary').v2
+
+exports.ImageUpload = async (file, folder, height, quality) => {
+    try {
+        const option = { folder }
+        if (height) {
+            option.height = height
+        }
+        if (quality) {
+            option.quality = quality
+        }
+
+        option.resource_type = "auto"
+
+        return await cloudinary.uploader(file.tempFilePath, option)
+    } catch (err) {
+        console.log(err)
+    }
+
+}
