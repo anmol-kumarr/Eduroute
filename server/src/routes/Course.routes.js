f8beb5a23684c6ba621c3ee60b7c0ed36f8696c9
Anmol kumar <anmolkumar31884@gmail.com>
2024-09-17 14:50:31 +0530

    testing phase changes

--- server/src/routes/Course.routes.js
+++ server/src/routes/Course.routes.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const { route } = require('./Profile.routes')
 const { isInstructor, auth, isAdmin } = require('../middleware/Auth.Mw')
-const { createCourse } = require('../controllers/Course')
+const { createCourse, getCourseDetails } = require('../controllers/Course')
 const { createCategories, showCategories } = require('../controllers/Categories')
 const { createSection, updateSection, deleteSection } = require('../controllers/Section')
-const { createSubSection } = require('../controllers/SubSection')
+const { createSubSection, updateSubsection, deleteSubSection } = require('../controllers/SubSection')
 
 const routes = express.Router()
-
+// testing done--------------------------------------------------------
 routes.post('/create/category',[auth,isAdmin],createCategories)
 
 routes.post('/create', [auth, isInstructor],createCourse)
+routes.get('/details/',[auth,isInstructor],getCourseDetails)
+// -----------------------------------------------------------------
 
 
+// testing done-----------------------------------------------
 routes.post('/create/section',[auth,isInstructor],createSection)
 routes.put('/update/section',[auth,isInstructor],updateSection)
-routes.delete('/section/:sectionId',[auth,isInstructor],deleteSection)
+routes.delete('/delete',[auth,isInstructor],deleteSection)
+// --------------------------------------------------------------------
 
-
+//testing done--------------------------------------------------------------
 routes.post('/create/subSection',[auth,isInstructor],createSubSection)
-routes.put('/update/subSection',[auth,isInstructor])
-
+routes.put('/update/subSection',[auth,isInstructor],updateSubsection)
+routes.delete('/delete/subSection',[auth,isInstructor],deleteSubSection)
+// -----------------------------------------------------------------------
 
 
 
