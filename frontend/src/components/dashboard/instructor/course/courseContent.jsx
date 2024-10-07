c841525b5299b858388c4d07af4b53745b55dded
Anmol kumar <anmolkumar31884@gmail.com>
2024-10-07 14:44:27 +0530

    refactor: drop down

--- frontend/src/components/dashboard/instructor/course/courseContent.jsx
+++ frontend/src/components/dashboard/instructor/course/courseContent.jsx
@@ -5,81 +5,66 @@ import { MdDelete } from "react-icons/md";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { useEffect, useRef, useState } from "react";
 const CourseContent = () => {
-    const dropDown = useRef(null)
+    // const dropDown = useRef([])
     const course = useSelector(state => state.course.course)
-    const [open, setOpen] = useState(false)
+    const [openSection, setOpenSection] = useState({})
+    const handleOpener=(id)=>{  
+        setOpenSection((prev)=>({
+            ...prev,    
+            [id]:!prev[id]
 
-    const isDropDownOpen = () => {
-        if (dropDown.current) {
-            console.log(dropDown)
-            return dropDown.current.open;
-        }
-
-        return false;
+        }))
     }
 
 
-    useEffect(() => {
-        const handleToggle = () => {
-            setOpen(isDropDownOpen());
-        };
+    return (<>
+        {course?.courseContent.length > 0 &&
+            <div className="bg-richblack-700 p-2 rounded-md">{
+                course?.courseContent?.map((section) => (
 
-        const currentDropDown = dropDown.current;
-        if (currentDropDown) {
-            currentDropDown.addEventListener('toggle', handleToggle);
+                    < details key={section._id} open={openSection[section._id] || false}  onClick={()=>handleOpener(section._id)}>
+                        <summary className="flex justify-between">
+                            <div className="flex gap-1  items-center">
+                                <RxDropdownMenu></RxDropdownMenu>
+                                {section.sectionName}
+                            </div>
+                            <div className="flex px-2">
+                                <div className=" flex gap-1 text-lg">
 
-            // Clean up the event listener when the component unmounts
-            return () => {
-                currentDropDown.removeEventListener('toggle', handleToggle);
-            };
-        }
-    }, []);
+                                    <button type="button">
+                                        <MdEdit></MdEdit>
+                                    </button>
+                                    <button className="border-r-[1.5px] border-richblack-600">
+                                        <MdDelete></MdDelete>
+                                    </button>
 
-    return (
-        <div className="bg-richblack-700 p-2 rounded-md">{
-            course?.courseContent?.map((section) => (
+                                </div>
 
-                < details key={section._id} ref={dropDown}>
-                    <summary className="flex justify-between">
-                        <div className="flex gap-1  items-center">
-                            <RxDropdownMenu></RxDropdownMenu>
-                            {section.sectionName}
-                        </div>
-                        <div className="flex px-2">
-                            <div className=" flex gap-1 text-lg">
+                                <div>
+                                    {
+                                        openSection[section._id] ? (
+                                            <IoIosArrowDown></IoIosArrowDown>
 
-                                <button type="button">
-                                    <MdEdit></MdEdit>
-                                </button>
-                                <button className="border-r-[1.5px] border-richblack-600">
-                                    <MdDelete></MdDelete>
-                                </button>
+                                        ) : (
+                                            <IoIosArrowUp></IoIosArrowUp>
+                                        )
+                                    }
+                                </div>
 
                             </div>
+                        </summary>
 
-                            <div>
-                                {
-                                    open ? (
-                                        <IoIosArrowDown></IoIosArrowDown>
 
-                                    ) : (
-                                        <IoIosArrowUp></IoIosArrowUp>
-                                    )
-                                }
-                            </div>
-
+                        <div>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur veritatis aliquid voluptatem deleniti, nostrum non totam quidem adipisci illo similique ipsa numquam saepe quod expedita. Eos, unde? Molestias, nesciunt accusantium labore voluptatum recusandae dicta?
                         </div>
-                    </summary>
+                    </details>
+                ))
 
-
-                    <div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur veritatis aliquid voluptatem deleniti, nostrum non totam quidem adipisci illo similique ipsa numquam saepe quod expedita. Eos, unde? Molestias, nesciunt accusantium labore voluptatum recusandae dicta?
-                    </div>
-                </details>
-            ))
-
+            }
+            </div >
         }
-        </div >
+    </>
     )
 }
 export default CourseContent
