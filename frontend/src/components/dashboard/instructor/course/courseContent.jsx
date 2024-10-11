7c583c0a79ed60c747d2c46465c294c958ff9149
Anmol kumar <anmolkumar31884@gmail.com>
2024-10-11 14:30:00 +0530

    modal problem solved

--- frontend/src/components/dashboard/instructor/course/courseContent.jsx
+++ frontend/src/components/dashboard/instructor/course/courseContent.jsx
@@ -8,7 +8,9 @@ import { IoAddCircleOutline } from "react-icons/io5";
 import SubSectionModal from "./subSectionModal";
 import Modal from "../../../modal";
 import { deleteSection } from "../../../../services/operation/course";
-
+import { IoArrowForward } from "react-icons/io5";
+import { changeState } from "../../../../redux/slice/courseSlice";
+import toast from "react-hot-toast";
 
 const CourseContent = ({ editSectionHandler }) => {
     const dispatch = useDispatch()
@@ -52,6 +54,9 @@ const CourseContent = ({ editSectionHandler }) => {
             id: id,
             type: 'Create',
             setSubSectionModal,
+            setEditSubSection,
+            setCreateSubSection,
+            setViewSubSection,
             create: true
         })
     }
@@ -64,16 +69,19 @@ const CourseContent = ({ editSectionHandler }) => {
         // console.log(subSectionValue.videoUrl)
         // console.log(subSectionValue.title)
 
-        // setViewSubSection({
-        //     type:'View',
-        //     view:true,
-        //     setSubSectionModal,
-        //     video:subSectionValue?.videoUrl,
-        //     title:subSectionValue?.title,
-        //     timeDuration:subSectionValue?.timeDuration,
-        //     description:subSectionValue?.description
-        // })
-        // setSubSectionModal(true)
+        setViewSubSection({
+            type: 'View',
+            view: true,
+            setSubSectionModal,
+            setEditSubSection,
+            setCreateSubSection,
+            setViewSubSection,
+            video: subSectionValue?.videoUrl,
+            title: subSectionValue?.title,
+            timeDuration: subSectionValue?.timeDuration,
+            description: subSectionValue?.description
+        })
+        setSubSectionModal(true)
 
     }
 
@@ -83,143 +91,168 @@ const CourseContent = ({ editSectionHandler }) => {
         const section = course.courseContent?.filter((section) => section._id === sectionId)
         const subSection = section[0]?.subSection?.filter((subSection) => subSection._id === subSectionId)[0]
         // .filter((section)=>section._id===sectionId)
-        console.log(subSection)
+        // console.log(subSection)
+        setSubSectionModal(true)
         setEditSubSection({
             type: 'Edit',
             edit: true,
             setSubSectionModal,
+            setEditSubSection,
+            setCreateSubSection,
+            setViewSubSection,
             video: subSection?.videoUrl,
             title: subSection?.title,
             timeDuration: subSection?.timeDuration,
             description: subSection?.description
         })
-        setSubSectionModal(true)
     }
 
 
+    // useEffect(() => {
+    //     if (!subSectionModal) {
+    //         setEditSubSection({});
+    //         setCreateSubSection({});
+    //         setViewSubSection({});
+    //     }
+    // }, [subSectionModal]);
+    // useEffect(()=>{
+    //     console.log(viewSubSection)
+    // },[viewSubSection])
 
-// useEffect(()=>{
-//     console.log(viewSubSection)
-// },[viewSubSection])
+    const nextHandler=()=>{
+        if(course?.courseContent?.length>0 && course?.courseContent?.subSection?.length>0)
+        {
+            dispatch(changeState(3))
+        }
+        else{
+            toast.error('Please add course content')
+        }
+    }
 
 
-return (
-    <>
-        {course?.courseContent.length > 0 &&
-            <div className="bg-richblack-700 p-2 mt-4 rounded-md">{
-                course?.courseContent?.map((section, index) => (
+    return (
+        <>
+            {course?.courseContent.length > 0 &&
+                <div className="bg-richblack-700 p-2 mt-4 rounded-md">{
+                    course?.courseContent?.map((section, index) => (
 
-                    < details className="" key={section._id} open={index === 0 ? true : openSection[section._id] ? true : false} onClick={() => handleOpener(section._id)}>
-                        <summary className="flex justify-between border-b border-richblack-600 pb-2">
-                            <div className="flex gap-1  items-center">
-                                <RxDropdownMenu></RxDropdownMenu>
-                                {section.sectionName}
-                            </div>
-                            <div className="flex  gap-1 px-2 items-center">
-                                <div className=" flex gap-1 text-lg">
-
-                                    <button type="button" onClick={() => editSectionHandler(section.sectionName, section._id)} >
-                                        <MdEdit></MdEdit>
-                                    </button>
-                                    <button onClick={() => setConfirmationModal({
-                                        textOne: `Delete ${section.sectionName} section`,
-                                        textTwo: 'All the lectures in this section will be deleted',
-                                        btnOneText: 'Confirm delete',
-                                        btnTwoText: 'Cancel',
-                                        btnOneHandler: () => deleteSectionHandler(section._id),
-                                        btnTwoHandler: () => setConfirmationModal(null)
-                                    })} className="border-r-[1.5px] pr-2 border-richblack-600">
-                                        <MdDelete></MdDelete>
-                                    </button>
-
+                        < details className="" key={section._id} open={index === 0 ? true : openSection[section._id] ? true : false} onClick={() => handleOpener(section._id)}>
+                            <summary className="flex justify-between border-b border-richblack-600 pb-2">
+                                <div className="flex gap-1  items-center">
+                                    <RxDropdownMenu></RxDropdownMenu>
+                                    {section.sectionName}
                                 </div>
+                                <div className="flex  gap-1 px-2 items-center">
+                                    <div className=" flex gap-1 text-lg">
 
-                                <div>
-                                    {
-                                        index === 0 ? (
-                                            openSection[section._id] ? (
+                                        <button type="button" onClick={() => editSectionHandler(section.sectionName, section._id)} >
+                                            <MdEdit></MdEdit>
+                                        </button>
+                                        <button onClick={() => setConfirmationModal({
+                                            textOne: `Delete ${section.sectionName} section`,
+                                            textTwo: 'All the lectures in this section will be deleted',
+                                            btnOneText: 'Confirm delete',
+                                            btnTwoText: 'Cancel',
+                                            btnOneHandler: () => deleteSectionHandler(section._id),
+                                            btnTwoHandler: () => setConfirmationModal(null)
+                                        })} className="border-r-[1.5px] pr-2 border-richblack-600">
+                                            <MdDelete></MdDelete>
+                                        </button>
 
-                                                <IoIosArrowDown></IoIosArrowDown>
-
-                                            ) : (
-                                                <IoIosArrowUp></IoIosArrowUp>
-
-                                            )
-                                        ) :
-                                            openSection[section._id] ? (
-                                                <IoIosArrowDown></IoIosArrowDown>
-
-
-                                            ) : (
-                                                <IoIosArrowUp></IoIosArrowUp>
-
-                                            )
-                                    }
-                                </div>
-
-                            </div>
-                        </summary>
-
-
-                        <div className="px-3">
-                            {
-                                section?.subSection && section?.subSection?.map((subSection) => (
-                                    <div onClick={() => viewSubSectionHandler(section._id, subSection._id)} key={subSection._id} className="mb-5 border-b border-richblack-600 p-2 flex justify-between">
-
-
-                                        <div className="flex items-center gap-1">
-                                            <RxDropdownMenu className="-mb-[2px]"></RxDropdownMenu>
-                                            {subSection.title}
-                                        </div>
-                                        <div className="flex gap-1">
-                                            <button onClick={() => editSubSectionHandler(subSection._id, section._id)} type="button">
-                                                <MdEdit></MdEdit>
-                                            </button>
-                                            <button type="button" onClick={() => setConfirmationModal({
-                                                textOne: `Delete ${subSection.title} Sub section`,
-                                                textTwo: 'The lectures in this section will be deleted',
-                                                btnOneText: 'Confirm delete',
-                                                btnTwoText: 'Cancel',
-                                                btnOneHandler: () => deleteSubSectionHandler(subSection._id),
-                                                btnTwoHandler: () => setConfirmationModal(null)
-                                            })}>
-                                                <MdDelete></MdDelete>
-                                            </button>
-                                        </div>
                                     </div>
 
-                                ))
-                            }
-                            <button onClick={() => createSubSectionHandler(section._id)} type="button" className=" text-yellow-100 flex gap-1 items-center px-2 text-sm my-2 py-1 border border-yellow-100 rounded-md">Add lecture
-                                <IoAddCircleOutline className="-mb-[3px] text-lg"></IoAddCircleOutline>
-                            </button>
-                        </div>
+                                    <div>
+                                        {
+                                            index === 0 ? (
+                                                openSection[section._id] ? (
 
-                    </details>
-                ))
+                                                    <IoIosArrowDown></IoIosArrowDown>
 
+                                                ) : (
+                                                    <IoIosArrowUp></IoIosArrowUp>
+
+                                                )
+                                            ) :
+                                                openSection[section._id] ? (
+                                                    <IoIosArrowDown></IoIosArrowDown>
+
+
+                                                ) : (
+                                                    <IoIosArrowUp></IoIosArrowUp>
+
+                                                )
+                                        }
+                                    </div>
+
+                                </div>
+                            </summary>
+
+
+                            <div className="px-3">
+                                {
+                                    section?.subSection && section?.subSection?.map((subSection) => (
+                                        <div onClick={() => viewSubSectionHandler(section._id, subSection._id)}
+                                            key={subSection._id} className="mb-5 border-b border-richblack-600 p-2 flex justify-between">
+
+
+                                            <div className="flex items-center gap-1">
+                                                <RxDropdownMenu className="-mb-[2px]"></RxDropdownMenu>
+                                                {subSection.title}
+                                            </div>
+                                            <div onClick={(e)=>e.stopPropagation()} className="flex gap-1">
+                                                <button onClick={() => editSubSectionHandler(subSection._id, section._id)} type="button">
+                                                    <MdEdit></MdEdit>
+                                                </button>
+                                                <button type="button" onClick={() => setConfirmationModal({
+                                                    textOne: `Delete ${subSection.title} Sub section`,
+                                                    textTwo: 'The lectures in this section will be deleted',
+                                                    btnOneText: 'Confirm delete',
+                                                    btnTwoText: 'Cancel',
+                                                    btnOneHandler: () => deleteSubSectionHandler(subSection._id),
+                                                    btnTwoHandler: () => setConfirmationModal(null)
+                                                })}>
+                                                    <MdDelete></MdDelete>
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                    ))
+                                }
+                                <button onClick={() => createSubSectionHandler(section._id)} type="button" className=" text-yellow-100 flex gap-1 items-center px-2 text-sm my-2 py-1 border border-yellow-100 rounded-md">Add lecture
+                                    <IoAddCircleOutline className="-mb-[3px] text-lg"></IoAddCircleOutline>
+                                </button>
+
+                            </div>
+
+                        </details>
+                    ))
+
+                }
+                    {
+                        subSectionModal && (
+
+                            createSubSection
+                                ? (<SubSectionModal
+                                    {...createSubSection}
+                                ></SubSectionModal>) :
+                                editSubSection ? (<SubSectionModal {...editSubSection}></SubSectionModal>) :
+                                    viewSubSection && (<SubSectionModal {...viewSubSection}></SubSectionModal>)
+
+                        )
+                    }
+                    {
+                        confirmationModal && <Modal {...confirmationModal}></Modal>
+
+                    }
+
+                    <div className="my-5 flex justify-center">
+                        <button type="button" onClick={()=>nextHandler()} className="bg-yellow-100 flex items-center gap-1 text-black font-semibold rounded-md px-3 py-1">Next <IoArrowForward className="-mb-[2px]"></IoArrowForward></button>
+                    </div>
+                </div >
             }
-                {
-                    subSectionModal && (
 
-                        createSubSection
-                            ? (<SubSectionModal
-                                {...createSubSection}
-                            ></SubSectionModal>) :
-                            editSubSection ?( <SubSectionModal {...editSubSection}></SubSectionModal> ):
-                                viewSubSection && (<SubSectionModal {...viewSubSection}></SubSectionModal>)
-
-                    )
-                }
-                {
-                    confirmationModal && <Modal {...confirmationModal}></Modal>
-
-                }
-            </div >
-        }
-
-    </>
-)
+        </>
+    )
 }
 export default CourseContent
 
--- frontend/src/components/dashboard/instructor/course/subSectionModal.jsx
+++ frontend/src/components/dashboard/instructor/course/subSectionModal.jsx
@@ -9,6 +9,9 @@ const SubSectionModal = ({
     type,
     create = false,
     setSubSectionModal,
+    setEditSubSection,
+    setCreateSubSection,
+    setViewSubSection,
     video,
     title,
     description,
@@ -16,26 +19,26 @@ const SubSectionModal = ({
     view = false,
     edit = false
 }) => {
-    const { register, handleSubmit, setValue,watch, getValues, formState: { errors } } = useForm()
+    const { register, handleSubmit, setValue, watch, getValues, formState: { errors } } = useForm()
     const loading = useSelector(state => state.course.loading)
-    const course=useSelector(state=>state.course.MyCourse)
+    const course = useSelector(state => state.course.MyCourse)
 
 
-const dispatch=useDispatch()
+    const dispatch = useDispatch()
 
     const submitHandler = (data) => {
-        if(edit){
+        if (edit) {
 
         }
-        else{
+        else {
             console.log(data)
-            const formData=new FormData()
-            formData.append('video',data.videoLecture)
-            formData.append('title',data.lectureTitle)
-            formData.append('description',data.lectureDescription)
-            formData.append('sectionId',id)
-            formData.append('courseId',course?._id)
-            formData.append('timeDuration',data.timeDuration)
+            const formData = new FormData()
+            formData.append('video', data.videoLecture)
+            formData.append('title', data.lectureTitle)
+            formData.append('description', data.lectureDescription)
+            formData.append('sectionId', id)
+            formData.append('courseId', course?._id)
+            formData.append('timeDuration', data.timeDuration)
 
 
             dispatch(createSubSection(formData))
@@ -44,28 +47,35 @@ const dispatch=useDispatch()
         }
 
     }
-    useEffect(()=>{
-        console.log('type',type)
-        console.log(video)
-        console.log(title)
-        console.log(description)
-        console.log(timeDuration)
-        console.log(view)
-        console.log('end')
-        console.log(setSubSectionModal)
-    
-        if(edit||view){
-            setValue('lectureTitle',title)
-            setValue('timeDuration',timeDuration)
-            setValue('lectureDescription',description)
-            setValue('videoLecture',video)
+    useEffect(() => {
+        // console.log('type', type)
+        // console.log(video)
+        // console.log(title)
+        // console.log(description)
+        // console.log(timeDuration)
+        // console.log(view)
+        // console.log('end')
+        // console.log(setSubSectionModal)
+
+        if (edit || view) {
+            setValue('lectureTitle', title)
+            setValue('timeDuration', timeDuration)
+            setValue('lectureDescription', description)
+            setValue('videoLecture', video)
         }
-    },[])
+    }, [])
 
 
     const lectureTitleValue = watch('lectureTitle');
     // console.log('title:',lectureTitleValue)
 
+
+    const modalHandler = () => {
+        setViewSubSection(null)
+        setEditSubSection(null)
+        setCreateSubSection(null)
+        setSubSectionModal(false)
+    }
 
     return (
         <div className="absolute top-0 -bottom-[100%] right-0 left-0 bg-richblack-900 bg-opacity-80">
@@ -74,7 +84,7 @@ const dispatch=useDispatch()
                 <div className="flex flex-col gap-2">
                     <div className="flex bg-richblack-800 rounded-md text-richblack-25 p-2 justify-between items-center ">
                         <p>{type} lecture</p>
-                        <button onClick={!loading && (() => setSubSectionModal(false))} type="button"><RxCross2></RxCross2></button>
+                        <button onClick={!loading && (modalHandler)} type="button"><RxCross2></RxCross2></button>
                     </div>
                     <hr className="text-richblack-600" />
                     <div>
@@ -87,7 +97,7 @@ const dispatch=useDispatch()
                                 setValue={setValue}
                                 getValues={getValues}
                                 errors={errors}
-                                video={video?video:null}
+                                video={video ? video : null}
                                 viewData={view ? video : null}
                                 editData={edit ? video : null}
                             ></UploadVideo>
@@ -107,7 +117,7 @@ const dispatch=useDispatch()
 
 
                                 <label htmlFor="timeDuration">Lecture duration</label><br />
-                                <input type="text" id="timeDuration" className="w-full rounded-md shadow-richblack-400 shadow-sm outline-none border-none my-1 bg-richblack-700 text-sm py-2 px-2" placeholder="Enter time duration " {...register('timeDuration', { required: true,valueAsNumber: true, })} />
+                                <input type="text" id="timeDuration" className="w-full rounded-md shadow-richblack-400 shadow-sm outline-none border-none my-1 bg-richblack-700 text-sm py-2 px-2" placeholder="Enter time duration " {...register('timeDuration', { required: true, valueAsNumber: true, })} />
                                 {
                                     errors.timeDuration && <span>Please enter valid lecture duration</span>
                                 }
