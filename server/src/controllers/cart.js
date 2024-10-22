5c299215d2bb18d1b56b52e8acf7992ef32b12c9
Anmol kumar <anmolkumar31884@gmail.com>
2024-10-22 22:54:04 +0530

    feat: get cart data controller added

--- server/src/controllers/cart.js
+++ server/src/controllers/cart.js
@@ -1,5 +1,38 @@
 const User = require('../models/User.Model')
 
+
+exports.getCart=async(req,res)=>{
+    try{
+
+        const userId=req.user.id
+
+        const findUser=await User.findById(userId).populate({
+            path:'cart',
+            populate:{
+                path:'intructor'
+            }
+        }).exec()
+        // console.log('cart',findUser)
+        return res.status(200).json({
+            success:true,
+            message:'Cart data fetched',
+            data:findUser.cart
+        })
+
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+            success:false,
+            message:'Internal server error'
+        })
+    }
+
+}
+
+
+
+
+
 exports.addToCart = async (req, res) => {
     try {
         const { courseId } = req.body
@@ -7,7 +40,7 @@ exports.addToCart = async (req, res) => {
         
         const userId = req.user.id
         if (!courseId) {
-            console.log('error')
+            // console.log('error')
             return res.status(203).json({
                 success: false,
                 message: 'CourseId is empty'
@@ -31,7 +64,7 @@ exports.addToCart = async (req, res) => {
         const response = await User.findByIdAndUpdate(userId, { $push: { cart: courseId } }, { new: true }).populate({
             path: 'cart'
         }).exec()
-        console.log(response)
+        console.log("user",response)
 
 
         if (response.length === 0) {
@@ -58,14 +91,16 @@ exports.addToCart = async (req, res) => {
 }
 
 
+
 exports.removeFromCart = async (req, res) => {
     try {
         const { courseId } = req.body
         const userId = req.user.id
 
-        const response = await Course.findByIdAndUpdate({ userId }, { $pull: { cart: courseId } }, { new: true }).populate({
+        const response = await User.findByIdAndUpdate({ userId }, { $pull: { cart: courseId } }, { new: true }).populate({
             path: 'cart'
         }).exec()
+
 
         if (response.length === 0) {
             return res.status(404).json({
--- server/src/routes/cart.routes.js
+++ server/src/routes/cart.routes.js
@@ -1,11 +1,12 @@
 const express =require('express')
 const { auth, isStudent } = require('../middleware/Auth.Mw')
-const { addToCart } = require('../controllers/cart')
+const { addToCart, getCart, removeFromCart } = require('../controllers/cart')
 
 const router=express.Router()
 
 
 router.post('/add-to-cart',[auth,isStudent],addToCart)
-router.put('/remove-from-cart',[auth,isStudent],addToCart)
+router.put('/remove-from-cart',[auth,isStudent],removeFromCart)
+router.get('/cart-data',[auth,isStudent],getCart)
 
 module.exports=router
