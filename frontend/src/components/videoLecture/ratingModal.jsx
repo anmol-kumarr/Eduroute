3d3bf8df8d5501ad90190b1194eac4327153d51f
Anmol kumar <anmolkumar31884@gmail.com>
2024-10-29 21:23:45 +0530

    refactor: rating modal use form hook added

--- frontend/src/components/videoLecture/ratingModal.jsx
+++ frontend/src/components/videoLecture/ratingModal.jsx
@@ -2,9 +2,16 @@ import { LiaTimesSolid } from "react-icons/lia";
 import { useSelector } from "react-redux";
 import { GoStar, GoStarFill } from "react-icons/go";
 import { useState } from "react";
+import { useForm } from "react-hook-form"
 const RatingModal = ({ setModal }) => {
     const user = useSelector(state => state.user.user)
     const [number, setNumber] = useState(0)
+    const { register, handleSubmit, setValue, getValues, formState: { errors } } = useForm()
+
+    const submitHandler = (data) => {
+        console.log(data, number)
+        console.log(errors)
+    }
     return (
         <div className="flex justify-center my-[20%]  items-center">
             <div className="w-96 bg-richblack-800 rounded-md overflow-hidden">
@@ -30,22 +37,33 @@ const RatingModal = ({ setModal }) => {
                         {[...Array(5)].map((_, i) => (
 
                             number >= i ? (
-                                <GoStarFill onClick={()=>setNumber(i)}></GoStarFill>
+                                <GoStarFill key={i} onClick={() => setNumber(i)}></GoStarFill>
                             ) : (
-                                <GoStar  onClick={()=>setNumber(i)}></GoStar>
+                                <GoStar key={i} onClick={() => setNumber(i)}></GoStar>
                             )
 
                         ))}
                     </div>
-                    <div className="p-1 flex flex-col gap-1 mt-2 items-center">
-                        <label className="text-richblack-200 text-sm" htmlFor="review">Add your Experience <span className="text-[#ff0000] text-start">*</span></label>
-                        <textarea rows={3} cols={40} className="p-1 rounded-md bg-richblack-700 outline-none  border-richblack-500" name="review" id="review"></textarea>
+                    <div className=" flex flex-col gap-1  items-center">
+                        <form onSubmit={handleSubmit(submitHandler)}>
+                            <div className="flex flex-col items-center p-1 text-sm">
+
+                                <label className="text-richblack-200 text-sm mb-1" htmlFor="review">Add your Experience <span className="text-[#ff0000] text-start">*</span></label>
+
+                                <textarea placeholder="" {...register('ratingdescription', { required: true })} rows={5} cols={50} className="p-1 rounded-md bg-richblack-700 outline-none  border-richblack-500" name="review" id="review"></textarea>
+
+                                <p>{errors.description && 'Error'}</p>
+
+
+                            </div>
+                            <div className="mb-5 mt-3 flex gap-2 justify-end">
+                                <button className="bg-richblack-400 px-2 py-1 rounded-md shadow-[2px_2px_0px_#2C333F]">Cancel</button>
+                                <button type="submit" className="text-black bg-yellow-100 px-2 py-1 rounded-md shadow-[2px_2px_0px_#FFE83D]">Submit</button>
+                            </div>
+                        </form>
                     </div>
 
-                    <div className="mb-5 mt-2 flex gap-2 justify-end">
-                        <button className="bg-richblack-400 px-2 py-1 rounded-md shadow-[2px_2px_0px_#2C333F]">Cancel</button>
-                        <button className="text-black bg-yellow-100 px-2 py-1 rounded-md shadow-[2px_2px_0px_#FFE83D]">Submit</button>
-                    </div>
+
                 </div>
             </div>
         </div>
