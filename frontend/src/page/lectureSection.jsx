d128dd056133b28fee2e7f43c14c68ccf1b3fb10
Anmol kumar <anmolkumar31884@gmail.com>
2024-10-29 18:55:01 +0530

    refactor: video lecture added

--- frontend/src/page/lectureSection.jsx
+++ frontend/src/page/lectureSection.jsx
@@ -5,18 +5,34 @@ import { apiConnector } from "../services/apiconnector"
 import { enrolledCourse } from "../services/api"
 import Spinner from "../components/spinner"
 import Sidebar from "../components/videoLecture/sidebar"
+import Footer from "../components/footer"
 
 const LectureSection = () => {
     const { courseId } = useParams()
+
     const [courseData, setCourseData] = useState([])
     const [loading, setLoading] = useState(false)
+    const [playVideo, setPlayVideo] = useState('')
+    const [subSection, setSubSection] = useState(null)
+    const[date,setDate]=useState('')
     const fetchLectureData = async () => {
         const api = `${enrolledCourse.getEnrolledCourse}/${courseId}`
         setLoading(true)
         try {
             const response = await apiConnector('GET', api, { courseId })
+
             setCourseData(response?.data?.data)
-            console.log(response)
+            setPlayVideo(response?.data?.data?.courseContent[0]?.subSection[0]?.videoUrl)
+            setSubSection(response?.data?.data?.courseContent[0]?.subSection[0])
+            
+            const createdAt = response?.data?.data?.courseContent[0]?.subSection[0]?.createdAt;
+            const date = new Date(createdAt).toLocaleString("en-IN", {
+                timeZone: "Asia/Kolkata",
+                year: "numeric",
+                month: "2-digit",
+                
+            });
+            setDate(date)
         }
         catch (err) {
             setCourseData([])
@@ -27,12 +43,29 @@ const LectureSection = () => {
     useEffect(() => {
         fetchLectureData()
     }, [])
+    useEffect(()=>{
+        const date = new Date(subSection?.createdAt).toLocaleString("en-IN", {
+            timeZone: "Asia/Kolkata",
+            year: "numeric",
+            month: "2-digit",
+            
+        })
+        setDate(date)
+    },[subSection])
+
+    
+    useEffect(() => {
+        // console.log(courseData)
+        if (!courseData.length === 0) setPlayVideo(courseData?.courseContent[0]?.subSection[0]?.videoUrl)
+
+        // console.log(courseData?.courseContent[0]?.subSection[0]?.videoUrl)
+    }, [courseData])
     return (
         <div className="text-white w-full">
-            <div className="bg-richblack-700 w-full">
+            <div className="bg-richblack-800 w-full">
                 <Header></Header>
             </div>
-
+            <hr className="text-richblack-600" />
 
             <div className="w-full">
 
@@ -52,10 +85,21 @@ const LectureSection = () => {
                                             <p>Please contact cutomer service</p>
                                         </div>
                                     ) : (
-                                        <div className="flex justify-between">
-                                            <Sidebar courseData={courseData}></Sidebar>
-                                            <div className="w-9/12 bg-richblack-400 h-5">
-                                                <Outlet></Outlet>
+                                        <div className="flex  justify-between">
+                                            <Sidebar setDate={setDate} setPlayVideo={setPlayVideo} setSubSection={setSubSection} courseData={courseData}></Sidebar>
+                                            <div className="min-h-[calc(100vh-5rem)] w-9/12 mx-auto flex flex-col justify-center">
+
+                                                <div className="my-2 max-h-[calc(100vh-5rem)] w-9/12 mx-auto flex justify-center">
+                                                    <video controls controlsList="nodownload" className="h-full w-96" src={playVideo}></video>
+                                                </div>
+
+                                                <div className="text-richblack-50 my-5 border border-richblack-600 p-3 rounded-md">
+                                                    <h2 className="font-inter text-lg font-semibold">{subSection?.title}</h2>
+                                                    <p className="font-inter">
+                                                        {subSection?.description}
+                                                    </p>
+                                                    <p>{date}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     )
@@ -64,6 +108,10 @@ const LectureSection = () => {
                         )
                 }
 
+            </div>
+
+            <div className="bg-richblack-800 border-t-[1px] border-richblack-700">
+                <Footer></Footer>
             </div>
         </div >
     )
